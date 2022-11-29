89d567b7f9d5836d9f3d6833a63871cd97187417
Nandankumar78 <nandanmsd7829@gmail.com>
2022-11-29 19:35:21 +0530

    coding

---
+++ palindrome.js
@@ -0,0 +1,12 @@
+// palindrome problem
+
+let str="naman"
+
+let count=0;
+
+for(let i=0;i<str.length/2;i++){
+    if(str[i]==str[N-1-i]){
+       count=1
+    }
+}
+
---
+++ prime.js
@@ -0,0 +1,10 @@
+//Prime problem
+
+let num=11;
+let count=0
+for(let i=1;i<num;i++){
+   if(num%2==0){
+      count++
+   }
+}
+    
